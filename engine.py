aaa009c213f4b280d42be31fa24735e93471ce88
Sheel <35765579+sushilr007@users.noreply.github.com>
2019-10-16 11:23:00 +0530

    Update engine.py
    Printing data in front of matched string.

--- engine.py
+++ engine.py
@@ -1,16 +1,34 @@
+# python script
 import pandas as pd
+import re
 from fuzzywuzzy import fuzz
-
-
-df = pd.read_csv("pathto/csvfile/.csv")
-
-
-a = df['Description '].values
-b = df['Article_description'].values
-c = df['Article Code '].values
-
-for i in range(len(b)):
-    for j in range(len(a)):
-        ratio = fuzz.token_sort_ratio(a[i], b[j]) #Will compare two strings for matching will return ratio
-        if ratio >= 90:
-            print(c[i],a[i],ratio)
+df = pd.read_excel("/path/to/excelfile")
+dff = df.replace(r'n',' ', regex=True) #removes 'n' from trail
+a = dff['Master_Description'].str.lower()
+b = dff['Description'].str.lower()
+c = dff['Master_Article_Code'].values
+acc = [None] * len(b)
+dis = [None] * len(b)
+po_dis = []
+for j in range(len(b)):
+    for i in range(len(a)):
+        ratio1 = fuzz.token_sort_ratio(a[i], b[j]) #Comparing two string using fuzzy
+        if  ratio1 >=90:
+#            acc.append(c[j])
+#            dis.append(a[j])
+            acc.insert(j,c[i])
+            dis.insert(j,a[i])
+# for x in range(len(acc)):
+#     if  acc[x] == None && dis[x] == None:
+#         acc.insert(x,"Not Found")
+#         dis.insert(x,"Not Found")
+if len(acc) == 0:
+    acc.append("Not found")
+    po_dis.append("Not found")
+    dfx= pd.DataFrame(list(zip(acc,po_dis)),columns =['Article code','Master Description'])
+    result = pd.concat([dff,dfx], axis=1, sort=True)
+    print("WRONG",dfx)
+else:
+    dfx= pd.DataFrame(list(zip(acc,dis)),columns =['Article code','Master Description'])
+    result = pd.concat([dff,dfx], axis=1, sort=True)#appending two dataframes
+    print(result)
